7c07a47a550477ff1309d43b4cb678a9135de853
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-12 19:09:10 +0530

    Objects

---
+++ Week 6/Basics/Object.js
@@ -0,0 +1,18 @@
+// let student={
+//     name:'Ayushman',
+//     age:19,
+//     marks:96
+// };
+// console.log(student);
+// console.log(student["name"]);
+// console.log(student.age);
+// console.log(student.marks);
+// student.marks=99;
+// console.log(student.marks);
+
+//random numbers from 1 to 10
+let num=Math.random();
+num*=10;
+num=Math.floor(num);
+num=num+1;
+console.log(num);
