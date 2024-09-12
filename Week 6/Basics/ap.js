a595dea72736914a5b85102c711aba2cf6bbc65c
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-12 16:17:29 +0530

    Basics

---
+++ Week 6/Basics/ap.js
@@ -0,0 +1,38 @@
+// for(let i=0;i<10;i++){
+//     console.log(i);
+// }
+//odd numbers
+// for(let i=1;i<=15;i+=2){
+//     console.log(i);
+// }
+// //even numbers
+// for(let i=2;i<=16;i+=2){
+//     console.log(i);
+// }
+
+// let n=prompt("Enter the number: ");
+// n=parseInt(n);
+// for(let i=n;i<=n*10;i+=n){
+//     console.log(i);
+// }
+// let x=5;
+// while(x!=50){
+//     console.log(x);
+//     x+=5;
+// }
+
+// const name="Ayushman";
+// let x=prompt("Enter the name: ");
+// while((x != name) || x=="Quit"){
+//     x=prompt("Enter the correct name: ");
+// }
+
+// let fruits=["Banana","Mango","Grapes","Orange",'Pineapple'];
+// for(let i=0;i<fruits.length;i++){
+//     console.log(i+1,fruits[i]);
+// }
+
+let name="ayushman";
+for(char of name){
+    console.log(char);
+}
