7aaa7e850959ce2fa8181da35aa418cccc13b1c8
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-16 02:24:05 +0530

    Event Bubbling

---
+++ Week 7/bubble.js
@@ -0,0 +1,18 @@
+let div=document.querySelector("div");
+let ul=document.querySelector("ul");
+let lis=document.querySelectorAll("li");
+
+div.addEventListener("click",function(){
+    console.log("div was clicked");
+});
+ul.addEventListener("click",function(){
+    event.stopPropagation();
+    console.log("ul was clicked");
+});
+
+for(li of lis){
+    li.addEventListener("click",function(){
+        event.stopPropagation();
+        console.log("li was clicked");
+    });
+}
