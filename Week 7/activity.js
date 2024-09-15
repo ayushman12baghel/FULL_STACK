ac73b60f15f79e5da9f4d9314ecd030900e7d574
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-15 16:55:52 +0530

    Small Activity

---
+++ Week 7/activity.js
@@ -0,0 +1,18 @@
+let button=document.querySelector("button");
+button.addEventListener("click",function(){
+    let h3=document.querySelector("h3");
+    let randomColor=getRandomColor();
+    h3.innerText=randomColor;
+    let div=document.querySelector("div");
+    div.style.backgroundColor=randomColor;
+    console.log("color updated");
+});
+
+function getRandomColor(){
+    let red=Math.floor(Math.random()*255);
+    let green=Math.floor(Math.random()*255);
+    let blue=Math.floor(Math.random()*255);
+
+    let color=`rgb(${red},${green},${blue})`;
+    return color;
+}
