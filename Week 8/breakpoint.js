0199ec6a458faa10b9b185184ebb072d8cda8d1f
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-17 14:29:06 +0530

    Breakpoints,Promises

--- Week 8/breakpoint.js
+++ Week 8/breakpoint.js
@@ -17,27 +17,51 @@ let h1=document.querySelector("h1");
 function  changeColor(color,delay){
     return new Promise((resolve,reject)=>{
         setTimeout(() => {
+            let num=Math.floor(Math.random()*5)+1;
+            if(num>3){
+                reject("promise rejected");
+            }
             h1.style.color=color;
+            console.log(color);
             resolve("color changed");
     },delay);
 });
 }
-changeColor("red",1000)
-.then(()=>{
-    console.log("red color was completed");
-    return changeColor("orange",1000);
-})
-.then(()=>{
-    console.log("orange color was completed");
-    return changeColor("green",1000);
-})
-.then(()=>{
-     console.log("green color was completed");
-    return changeColor("blue",1000);
-})
-.then(()=>{
-    console.log("blue color was completed");
-});
+
+async function demo(){
+    try{
+        await changeColor("red",1000);
+        await changeColor("orange",1000);
+        await changeColor("green",1000);
+        await changeColor("blue",1000);
+        await changeColor("yellowgreen",1000);
+
+    }
+    catch(err){
+        console.log(err);
+    }
+            
+    let a=5;
+    console.log(a);
+
+}
+
+// changeColor("red",1000)
+// .then(()=>{
+//     console.log("red color was completed");
+//     return changeColor("orange",1000);
+// })
+// .then(()=>{
+//     console.log("orange color was completed");
+//     return changeColor("green",1000);
+// })
+// .then(()=>{
+//      console.log("green color was completed");
+//     return changeColor("blue",1000);
+// })
+// .then(()=>{
+//     console.log("blue color was completed");
+// });
 
 // changeColor("red",1000,()=>{
 //     changeColor("orange",1000,()=>{
@@ -92,3 +116,36 @@ changeColor("red",1000)
 // .catch(()=>{
 //     console.log("promise rejected");
 // });
+
+// async function greet(){
+//     return "hello world";
+// }
+// greet()
+// .then((result)=>{
+//     console.log("promise was successful");
+//     console.log(result);
+// })
+// .catch((error)=>{
+//     console.log("error caught");
+//     console.log(error);
+// });
+
+// let demo=async()=>{
+//     return 5;
+// }
+
+// function getNumber(){
+//     return new Promise((resolve,reject)=>{
+//        setTimeout(()=>{
+//         let num=Math.floor(Math.random()*10)+1;
+//         console.log(num);
+//         resolve();
+//        },1000);
+//     })
+// }
+// async function demo(){
+//     await getNumber();
+//     await getNumber();
+//     await getNumber();
+//     getNumber();
+// }
