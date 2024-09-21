a06909cacb64623c879fe01ec408276a20a10908
ayushman12baghel <ayushman12baghel@gmail.com>
2024-09-21 18:31:06 +0530

    Node JS Basics

---
+++ Week9/Backend/math.js
@@ -0,0 +1,14 @@
+module.exports.sum=(a,b)=>a+b;
+const mul=(a,b)=>a*b;
+const g=9.8;
+module.exports.PI=3.14;
+
+// module.exports=123;
+// let obj={
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     PI:PI,
+// };
+
+// module.exports=obj;
---
+++ Week9/Backend/script.js
@@ -0,0 +1,14 @@
+// let n=5;
+// for(let i=0;i<n;i++){
+//     console.log(i);
+// }
+
+// console.log(process.argv);
+// let args=process.argv;
+// for(let i=2;i<args.length;i++){
+//     console.log("hello ",args[i]);
+// }
+
+const math=require("./math");
+console.log(math.sum(2,2));
+console.log(math.PI);
